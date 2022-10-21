d80bd2de1204bd570fbf7325dbd389684b4e6c89
Dmytro Zemnytskyi <pragmasoft@gmail.com>
2022-10-21 18:41:55 +0300

    reformatting

--- src/main/java/ua/com/pragmasoft/humane/controller/HumaneChatSocket.java
+++ src/main/java/ua/com/pragmasoft/humane/controller/HumaneChatSocket.java
@@ -22,65 +22,64 @@ import ua.com.pragmasoft.humane.payload.MsgPackHumaneEncoder;
 import ua.com.pragmasoft.humane.payload.PlaintextMsg;
 
 @ServerEndpoint(value = "/api/{channel}", decoders = {MsgPackHumaneDecoder.class},
-        encoders = {MsgPackHumaneEncoder.class}, subprotocols = {"v1.k1te.chat"})
+    encoders = {MsgPackHumaneEncoder.class}, subprotocols = {"v1.k1te.chat"})
 @ApplicationScoped
 public class HumaneChatSocket {
 
-    private static final String USER_ID = "k1te.userId";
+  private static final String USER_ID = "k1te.userId";
 
-    Map<String, Session> sessions = new ConcurrentHashMap<>();
+  Map<String, Session> sessions = new ConcurrentHashMap<>();
 
-    @OnOpen
-    public void onOpen(Session session, @PathParam("channel") String channel) {
-        Log.debugf("client connected to channel %s", channel);
+  @OnOpen
+  public void onOpen(Session session, @PathParam("channel") String channel) {
+    Log.debugf("client connected to channel %s", channel);
+  }
+
+  @OnClose
+  public void onClose(Session session, @PathParam("channel") String channel) {
+    Log.debugf("client disconnected from channel %s", channel);
+  }
+
+  @OnError
+  public void onError(Session session, @PathParam("channel") String channel, Throwable t) {
+    Log.errorf(t, "connection error on channel %s", channel);
+  }
+
+  @OnMessage
+  public void onMessage(HumaneMsg message, @PathParam("channel") String channel, Session session) {
+    // Decided not to use switch pattern matching until it releases from preview
+    final short type = message.type();
+    switch (type) {
+      case HumaneMsg.CONNECTED -> this.onConnected((ConnectedMsg) message, session);
+      case HumaneMsg.DISCONNECTED -> this.onDisconnected((DisconnectedMsg) message, session);
+      case HumaneMsg.PLAINTEXT -> this.onPlaintext((PlaintextMsg) message, session);
+      default -> throw new IllegalStateException("Unsupported type %X".formatted(type));
     }
+  }
 
-    @OnClose
-    public void onClose(Session session, @PathParam("channel") String channel) {
-        Log.debugf("client disconnected from channel %s", channel);
-    }
+  void onConnected(ConnectedMsg m, Session session) {
+    String userId = m.userId();
+    Log.debugf("Connected %s", userId);
+    this.sessions.put(userId, session);
+    session.getUserProperties().put(USER_ID, userId);
+  }
 
-    @OnError
-    public void onError(Session session, @PathParam("channel") String channel, Throwable t) {
-        Log.errorf(t, "connection error on channel %s", channel);
+  void onDisconnected(DisconnectedMsg m, Session session) {
+    Log.debugf("Disconnected %s", m.userId());
+    var removed = this.sessions.remove(m.userId(), session);
+    if (!removed) {
+      Log.warnf("Session does not match when removed for user %s", m.userId());
     }
+    try {
+      session.close();
+    } catch (IOException e) {
+      Log.error("Exception while closing session with user " + m.userId(), e);
+    }
+  }
 
-    @OnMessage
-    public void onMessage(HumaneMsg message, @PathParam("channel") String channel,
-            Session session) {
-        // Decided not to use switch pattern matching until it releases from preview
-        final short type = message.type();
-        switch (type) {
-            case HumaneMsg.CONNECTED -> this.onConnected((ConnectedMsg) message, session);
-            case HumaneMsg.DISCONNECTED -> this.onDisconnected((DisconnectedMsg) message, session);
-            case HumaneMsg.PLAINTEXT -> this.onPlaintext((PlaintextMsg) message, session);
-            default -> throw new IllegalStateException("Unsupported type %X".formatted(type));
-        }
-    }
-
-    void onConnected(ConnectedMsg m, Session session) {
-        String userId = m.userId();
-        Log.debugf("Connected %s", userId);
-        this.sessions.put(userId, session);
-        session.getUserProperties().put(USER_ID, userId);
-    }
-
-    void onDisconnected(DisconnectedMsg m, Session session) {
-        Log.debugf("Disconnected %s", m.userId());
-        var removed = this.sessions.remove(m.userId(), session);
-        if (!removed) {
-            Log.warnf("Session does not match when removed for user %s", m.userId());
-        }
-        try {
-            session.close();
-        } catch (IOException e) {
-            Log.error("Exception while closing session with user " + m.userId(), e);
-        }
-    }
-
-    void onPlaintext(PlaintextMsg m, Session session) {
-        Log.debugf("Plaintext '%s'", m.payload());
-        var f = session.getAsyncRemote().sendObject(m);
-        Log.debug(f.getClass().getName());
-    }
+  void onPlaintext(PlaintextMsg m, Session session) {
+    Log.debugf("Plaintext '%s'", m.payload());
+    var f = session.getAsyncRemote().sendObject(m);
+    Log.debug(f.getClass().getName());
+  }
 }
